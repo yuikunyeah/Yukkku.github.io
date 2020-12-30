b4e72ea59bfdc02ff462cdd52c1b0778f694b19e
Yukkku <76182771+Yukkku@users.noreply.github.com>
2020-12-31 07:09:34 +0900

    Update main.js

--- main.js
+++ main.js
@@ -82,14 +82,17 @@ function menuClose() {
   document.getElementById("gut").style.setProperty('--gut', '100%');
 }
 
-setInterval( function () {
-  if( mousey - window.pageYOffset < 80 ){
-    document.getElementById("top").style.setProperty('--top', 'calc(100% - 80px)');
-  }else{
-    if( window.pageYOffset > 80 ){
-      document.getElementById("top").style.setProperty('--top', '100%');
+let ua = navigator.userAgent
+if (!(ua.indexOf('iPhone') > -1 || ua.indexOf('iPad') > -1 || ua.indexOf('Android') > -1 )) {
+  setInterval( function () {
+    if( mousey - window.pageYOffset < 80 ){
+      document.getElementById("top").style.setProperty('--top', 'calc(100% - 80px)');
     }else{
-      document.getElementById("top").style.setProperty('--top','calc(100% - ' + ( 80 - window.pageYOffset ) + 'px)');
+      if( window.pageYOffset > 80 ){
+        document.getElementById("top").style.setProperty('--top', '100%');
+      }else{
+        document.getElementById("top").style.setProperty('--top','calc(100% - ' + ( 80 - window.pageYOffset ) + 'px)');
+      }
     }
-  }
-},100)
+  },100)
+}
